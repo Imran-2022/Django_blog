57ee6ceea5ed259539fe14cbdfb8df783224119d
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-10 00:24:46 +0600

    Working with Base HTML and Forms

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'categories',
     'posts',
     'profille',
+     "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- author/forms.py
+++ author/forms.py
@@ -3,4 +3,10 @@ from . models import Author
 
 class AuthorForm(forms.ModelForm):
     class Meta:
+        model = Author
+        fields='__all__'
+        # sb field nea ekta form create kre dbe...
+        # exclude= []
+        # fields=['name','abc]
+
         
