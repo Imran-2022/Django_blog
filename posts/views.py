4f0e5f5386e4f976991415147495eec2de1ce1f3
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-16 02:53:40 +0600

    edit post and logout

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from . import forms
 from . import models
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required
 def add_post(request):
     if request.method=='POST': # user post request koreche
         post_form=forms.PostForm(request.POST) # user er post request data ekhane capture krlm.
@@ -15,7 +18,7 @@ def add_post(request):
         post_form=forms.PostForm()
     return render(request, 'add_post.html',{'form':post_form})
 
-
+@login_required
 def edit_post(request,id):
     
     post = models.Post.objects.get(pk=id)
@@ -25,11 +28,12 @@ def edit_post(request,id):
     if request.method=='POST': 
         post_form=forms.PostForm(request.POST,instance=post) 
         if post_form.is_valid(): 
+            post_form.instance.author=request.user
             post_form.save()
             return redirect('homepage') 
     return render(request, 'add_post.html',{'form':post_form})
 
-
+@login_required
 def delete_post(request,id):
     post = models.Post.objects.get(pk=id)
     post.delete()
