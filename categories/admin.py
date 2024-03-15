e66c1aba427d092d42573672f0f56ccff65bd9c4
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-16 03:19:19 +0600

    Prepopulated Field

--- categories/admin.py
+++ categories/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Category)
+# admin.site.register(models.Category)
+
+# admin k jei class ta modify kre - model admin !
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
+    list_display=['name','slug']
+
+
+admin.site.register(models.Category,CategoryAdmin)
